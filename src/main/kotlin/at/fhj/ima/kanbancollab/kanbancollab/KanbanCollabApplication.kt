0dbca7b638b55c47fa4e79ae7e147c279d00e133
Martin Schweighofer <martin.schweighofer94@gmx.at>
2020-05-21 10:42:54 +0200

    Initialization KanbanCollab

---
+++ src/main/kotlin/at/fhj/ima/kanbancollab/kanbancollab/KanbanCollabApplication.kt
@@ -0,0 +1,11 @@
+package at.fhj.ima.kanbancollab.kanbancollab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KanbanCollabApplication
+
+fun main(args: Array<String>) {
+	runApplication<KanbanCollabApplication>(*args)
+}
---
+++ src/test/kotlin/at/fhj/ima/kanbancollab/kanbancollab/KanbanCollabApplicationTests.kt
@@ -0,0 +1,13 @@
+package at.fhj.ima.kanbancollab.kanbancollab
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KanbanCollabApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
