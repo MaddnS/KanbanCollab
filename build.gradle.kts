fb441e6d55095f410f81c6145b906a67ce79bbc2
Peter <peter.feldgrill@edu.fh-joanneum.at>
2020-05-21 15:40:20 +0200

    First Commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,12 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	// mysql
+	implementation("mysql:mysql-connector-java")
+	// jsp
+	compileOnly("org.apache.tomcat.embed:tomcat-embed-jasper")
+	implementation("javax.servlet:jstl")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
